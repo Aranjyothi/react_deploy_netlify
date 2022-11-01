a9a0232a4d9fb5117239712bad6e0817310ce38b
jyothipropelo <88416663+Aranjyothi@users.noreply.github.com>
2022-11-01 19:56:17 -0400

    commits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import FavoriteBook from './pages/FavoriteBook';
+import HomePage from './pages/HomePage';
+import { useState } from 'react'
+import BookDetails from './pages/BookDetails';
+
 
 function App() {
+  const [favorite, setFavorite] = useState([])
+  function addToFavorites(item) {
+    setFavorite([...favorite, item])
+    console.log(item)
+  }
+  function removeFromFavorites(item) {
+    setFavorite(favorite.filter((f) => {
+      if (item.id !== f.id) {
+        return true
+      } else {
+        return false
+      }
+    }))
+  }
+  function addMoreToFavorites(item) {
+    setFavorite([...favorite, item])
+  }
+  //  const addMoreToFavorites = (more,id) =>{
+  //   console.log(more.id)
+  //   let newUpdatedFavorites = favorite.map(m=>{
+  //     if(m.itemid === id){
+  //       return{
+  //         ...m,
+  //         rating:more.rating,
+  //         notes:more.notes
+  //       }
+  //     } else{
+  //       return m
+  //     }
+  //   })
+  //   favorite.map(m=> m.itemid === id ?{...m,...more}:m)
+  //   setFavorite(newUpdatedFavorites)
+  //  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage favorite={favorite} addToFavorites={addToFavorites} />} />
+        <Route path='/favorites' element={<FavoriteBook favorite={favorite} removeFromFavorites={removeFromFavorites} />} />
+        <Route path='/favorites/:id' element={<BookDetails favorite={favorite} addMoreToFavorites={addMoreToFavorites} />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+
     </div>
+
   );
 }
 
